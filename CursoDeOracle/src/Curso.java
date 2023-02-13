b25d2880f569e76d58d415fa725c6f3b49ea7573
Eduardo Landa <lalitomanlanda@gmail.com>
2023-02-12 19:11:13 -0600

    mi commit Oracle

---
+++ CursoDeOracle/src/Curso.java
@@ -0,0 +1,18 @@
+public class Curso {
+    public static void main(String[] args) {
+
+        // Texto
+        String sTexto = "palabra o palabra y palabra";
+        // Texto que vamos a buscar
+        String sTextoBuscado = "palabra";
+        // Contador de ocurrencias
+        int contador = 0;
+
+        while (sTexto.indexOf(sTextoBuscado) > -1) {
+            sTexto = sTexto.substring(sTexto.indexOf(
+                    sTextoBuscado)+sTextoBuscado.length(),sTexto.length());
+            contador++;
+        }
+        System.out.println (contador);
+    }
+}
